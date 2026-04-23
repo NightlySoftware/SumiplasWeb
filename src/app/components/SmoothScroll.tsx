649728e5cf40cb35dfdc8394fbbff680701b9d40
Apocalix <me@apocalix.dev>
2026-04-23 10:20:00 -0600

    Fix Lenis React type mismatch on build

--- src/app/components/SmoothScroll.tsx
+++ src/app/components/SmoothScroll.tsx
@@ -3,8 +3,15 @@
 import React, { PropsWithChildren } from 'react';
 import { ReactLenis } from '@studio-freight/react-lenis';
 
+const LenisRoot = ReactLenis as unknown as React.ComponentType<
+  PropsWithChildren<{
+    options: { duration: number };
+    root: boolean;
+  }>
+>;
+
 function SmoothScroll({ children }: PropsWithChildren): JSX.Element {
-  return <ReactLenis options={{ duration: 1 }} root>{children}</ReactLenis>;
+  return <LenisRoot options={{ duration: 1 }} root>{children}</LenisRoot>;
 }
 
 export default SmoothScroll;
